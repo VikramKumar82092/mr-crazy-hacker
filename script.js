b8e64d4f45601711b792ec066444cf7f444ec944
VikramKumar82092 <128372205+VikramKumar82092@users.noreply.github.com>
2025-12-18 23:46:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,19 @@
-// Terminal typing effect
-const text = "root@mr-crazy-hacker:~# Access Granted...\nLoading Ethical Hacking Modules...";
+const text =
+"root@mr-crazy-hacker:~# Access Granted...\n" +
+"Loading Vikram's Cyber Security Modules...\n" +
+"Welcome to Mr Crazy Hacker Website 🚀";
+
 let i = 0;
 const terminal = document.getElementById("terminalText");
 
 setInterval(() => {
-    if (i < text.length) {
-        terminal.innerHTML += text.charAt(i);
-        i++;
-    }
-}, 50);
+  if (i < text.length) {
+    terminal.innerHTML += text.charAt(i);
+    i++;
+  }
+}, 40);
 
-// Theme toggle
-const toggle = document.getElementById("themeToggle");
-toggle.onclick = () => {
-    document.body.classList.toggle("light");
-    document.body.classList.toggle("dark");
+document.getElementById("themeToggle").onclick = () => {
+  document.body.classList.toggle("light");
+  document.body.classList.toggle("dark");
 };
